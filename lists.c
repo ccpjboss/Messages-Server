5b428d1e52822fe791694f169e3cc8fc538ca25a
Me <jpccastilho1999@gmail.com>
2020-05-07 12:44:09 +0100

    Fixed extra \n

--- lists.c
+++ lists.c
@@ -13,10 +13,11 @@ void printUtilizadors(Utilizador_t *node)
   Utilizador_t *atual = node;
   while (atual != NULL)
   {
-    printf("%s \t %s \n", atual->email, atual->password);
+    printf("%s  %s \n", atual->email, atual->password);
     atual = atual->proximo;
   }
 }
+
 void insereUser(int socket_fd, Lista_t *list)
 {
   Utilizador_t *new = (Utilizador_t *)malloc(sizeof(Utilizador_t));
@@ -27,20 +28,24 @@ void insereUser(int socket_fd, Lista_t *list)
   char buff[256];
   char buff2[20];
 
-  write(socket_fd,msg_send1,sizeof(msg_send1));
-  bzero(buff,256);
-  read(socket_fd,buff,sizeof(buff));
-  buff[strcspn(buff,"\n")] = 0;
-  strcpy(new->email,buff);
+  write(socket_fd,msg_send1,sizeof(msg_send1)); //Envia mensagem de input para o cliente
+  bzero(buff,256); //Coloca o buffer a 0
+  read(socket_fd,buff,sizeof(buff)); //Le o input do cliente
+  strcpy(new->email,buff); //Copia para a variavel da struct
+
+  char *p = strchr(new->email, '\n');  //Encontra o \n
+  if (p != NULL) *p = '\0'; //Remove o \n extra
 
   write(socket_fd,msg_send2,sizeof(msg_send2));
   bzero(buff2,20);
   read(socket_fd,buff2,sizeof(buff2));
-  buff2[strcspn(buff2,"\n")] = 0;
   strcpy(new->password,buff2);
 
+  p = strchr(new->password, '\n'); 
+  if (p != NULL) *p = '\0';
+
   new->admin = false;
-new->codigo_user = 1; //TODO: Think about user code
+  new->codigo_user = 1; //TODO: Think about user code
   new->proximo = NULL;
 
   if ( list->cabeca_u == NULL)
