8e6e5fed31275ff88f98f58481b7ca831b6c90ad
Me <jpccastilho1999@gmail.com>
2020-05-07 16:10:57 +0100

    Fixed \n version 2

--- lists.c
+++ lists.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-void inicia_lista(Lista_t *const list)
+void inicia_lista(Lista_t * list)
 {
   list->cabeca_m = NULL;
   list->cabeca_u = NULL;
@@ -18,37 +18,46 @@ void printUtilizadors(Utilizador_t *node)
   }
 }
 
+bool verificaDupUser(Lista_t *list, char *email)
+{
+  bool dup = false;
+  Utilizador_t *atual = list->cabeca_u;
+  while (atual != NULL)
+  {
+    if (strcmp(email, atual->email) == 0)
+      dup = true;
+    atual = atual->proximo;
+  }
+  return dup;
+}
+
 void insereUser(int socket_fd, Lista_t *list)
 {
   Utilizador_t *new = (Utilizador_t *)malloc(sizeof(Utilizador_t));
   Utilizador_t *atual = list->cabeca_u;
 
-  char msg_send1 [] = "Insira o seu email: ";
-  char msg_send2 [] = "Insira a palavra passe: ";
+  char msg_send1[] = "Insira o seu email: ";
+  char msg_send2[] = "Insira a palavra passe: ";
   char buff[256];
   char buff2[20];
 
-  write(socket_fd,msg_send1,sizeof(msg_send1)); //Envia mensagem de input para o cliente
-  bzero(buff,256); //Coloca o buffer a 0
-  read(socket_fd,buff,sizeof(buff)); //Le o input do cliente
-  strcpy(new->email,buff); //Copia para a variavel da struct
+  write(socket_fd, msg_send1, sizeof(msg_send1)); //Envia mensagem de input para o cliente
+  bzero(buff, 256);                               //Coloca o buffer a 0
+  read(socket_fd, buff, sizeof(buff));            //Le o input do cliente
+  buff[strcspn(buff, "\r\n")] = 0;
+  strcpy(new->email, buff); //Copia para a variavel da struct
 
-  char *p = strchr(new->email, '\n');  //Encontra o \n
-  if (p != NULL) *p = '\0'; //Remove o \n extra
-
-  write(socket_fd,msg_send2,sizeof(msg_send2));
-  bzero(buff2,20);
-  read(socket_fd,buff2,sizeof(buff2));
-  strcpy(new->password,buff2);
-
-  p = strchr(new->password, '\n'); 
-  if (p != NULL) *p = '\0';
+  write(socket_fd, msg_send2, sizeof(msg_send2));
+  bzero(buff2, 20);
+  read(socket_fd, buff2, sizeof(buff2));
+  buff2[strcspn(buff2, "\r\n")] = 0;
+  strcpy(new->password, buff2);
 
   new->admin = false;
   new->codigo_user = 1; //TODO: Think about user code
   new->proximo = NULL;
 
-  if ( list->cabeca_u == NULL)
+  if (list->cabeca_u == NULL) //Se a cabeça for igual a NULL então new vai ser o primeiro nó
   {
     list->cabeca_u = new;
     new->proximo = NULL;
--- lists.h
+++ lists.h
@@ -37,5 +37,6 @@ typedef struct Lista
 void inicia_lista(Lista_t *const list);
 void printUtilizadors(Utilizador_t *const node);
 void insereUser(int socket_fd, Lista_t *list);
+bool verificaDupUser(Lista_t *list, char *email);
 
 #endif
